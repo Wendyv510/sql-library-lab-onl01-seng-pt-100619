167798e249d529af168f578f95410027e5368239
Wendy Harris <wendyv510@hotmail.com>
2019-12-30 19:29:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,11 +24,16 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name AS authors_name, subgenres.name AS subgenres_name
+   FROMa series
+   INNER JOIN authors 
+      ON series.author_id = authors.id
+   INNER JOIN subgenres 
+      ON series.subgenres.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  ""
 end
 
 def select_character_names_and_number_of_books_they_are_in
